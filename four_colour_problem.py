d88323d82c33e7e71e0cd8caa83a1747251116cd
mikeyPower <powerm3@tcd.ie>
2020-05-17 21:30:28 +0100

    fixed matrix

--- four_colour_problem.py
+++ four_colour_problem.py
@@ -5,6 +5,8 @@ import networkx as nx
 from osgeo import gdal, ogr
 import momepy
 import pysal as ps
+import numpy as np
+from pylab import figure, scatter, show
 
 
 # set the filepath and load in a shapefile
@@ -15,13 +17,12 @@ map_df = gpd.read_file(fp)
 # Construct queen weights from the dataframe
 Q_w = ps.lib.weights.Queen.from_dataframe(map_df)
 
-for i in Q_w:
-	print(i)
+# Dense matrix describing all of the pairwise relationships
 
-#dataframe = ps.pdio.read_files(shp_path)
+Wmatrix, ids = Q_w.full()
 
-
-
+print(Wmatrix)
+#nx.draw_networkx_edge_labels(G, pos=layout)
 
 
 # check data type so we can see that this is not a normal dataframe, but a GEOdataframe
@@ -42,78 +43,44 @@ map_df['colour'] = None # add color coloumn
 
 
 map_df["NEIGHBORS"] = None  # add NEIGHBORS column
-
+nodes_edges=[]
+#neighbours_dict={}
 for index, county in map_df.iterrows():   
+    neighbours_dict={}
     # get 'not disjoint' countries
-    neighbors = map_df[~map_df.geometry.disjoint(county.geometry)].ENGLISH.tolist()
+    neighbors = map_df[~map_df.geometry.disjoint(county.geometry)].OBJECTID.tolist()
     # remove own name from the list
-    neighbors = [ name for name in neighbors if county.ENGLISH != name ]
+    neighbors = [ name for name in neighbors if county.OBJECTID != name ]
     # add names of neighbors as NEIGHBORS value
-    map_df.at[index, "NEIGHBORS"] = ", ".join(neighbors)
+    #map_df.at[index, "NEIGHBORS"] = ", ".join(neighbors)
+    # add weight of 1 to all members of neighbours
+    for i in neighbors:
+        neighbours_dict[i]=1
+    nodes_edges.append((county.OBJECTID,neighbours_dict))
 
-for field in map_df:
-	print(field)
 
-print(map_df)
+for i in nodes_edges:
+	print(i)
+
+# create a 26 * 26 matrix [26][26] having each neighbour with 1.0 and the rest as 0.0
+matrix = [[0.0 for x in range(26)] for y in range(26)] 
+for i in nodes_edges:
+	dict = i[1]
+	for j in dict:
+		matrix[i[0]-1][j-1] = 1.0
+
+print(matrix)
+G = nx.from_numpy_matrix(np.matrix(Wmatrix), create_using=nx.DiGraph)
+layout = nx.spring_layout(G)
+nx.draw(G,layout)
+plt.show()
+
+
+
+
+
+#print(map_df)
 #print(map_df.loc[map_df['ENGLISH'] == 'CORK'])
-
-#for index, row in map_df.iterrows();
-
-
-	# See if any of it's neighbours has the same colour
-#	for i in row[-1]:
-
-		# Obtain the neighbours row based on the county's name
-#		new_row = map_df.loc[map_df['ENGLISH'] == i])
-		# see if colour of neighbour is the same
-
-#		if(new_row[-2] == row[-2]):
-			# if the same change colour of row[-2]
-#		else:
-			# check next neighbouring county
-#			continue
-
-
-# save GeoDataFrame as a new file
-#map_df.to_file("newfile.shp")
-
-
-#colour=['r', 'g', 'b', 'y']
-
-
-# Converting shapefile to graph
-#G=nx.read_shp(fp) 
-#pos = {k: v for k,v in enumerate(G.nodes())}
-#X=nx.Graph() #Empty graph
-#X.add_nodes_from(pos.keys()) #Add nodes preserving coordinates
-#l=[set(x) for x in G.edges()] #To speed things up in case of large objects
-#edg=[tuple(k for k,v in pos.items() if v in sl) for sl in l] #Map the G.edges start and endpoints onto pos
-#nx.draw_networkx_nodes(X,pos,node_size=100,node_color='r')
-#X.add_edges_from(edg)
-#nx.draw_networkx_edges(X,pos)
-#plt.xlim(450000, 470000) #This changes and is problem specific
-#plt.ylim(430000, 450000) #This changes and is problem specific
-#plt.xlabel('X [m]')
-#plt.ylabel('Y [m]')
-#plt.title('From shapefiles to NetworkX')
-#print(X)
-
-    # generate graph from GeoDataFrame of LineStrings
-#net = nx.Graph()
-#net.graph['crs'] = map_df.crs
-#fields = list(map_df.columns)
-
-#for index, row in map_df.iterrows():
-#    first = row.geometry.coords[0]
-#    last = row.geometry.coords[-1]
-
-#    data = [row[f] for f in fields]
-#    attributes = dict(zip(fields, data))
-#    net.add_edge(first, last, **attributes)
-
-#print(net)
-
-
 
 
 # Print map with alternate 4 colours
